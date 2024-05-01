3fade1791416740099ffd8caed5b4327de19c600
Илья <pandas1987bag@mail.ru>
2024-05-01 14:13:16 +0300

    решение задачи

--- src/task/methods/Methods.java
+++ src/task/methods/Methods.java
@@ -9,10 +9,13 @@ public class Methods {
         String string = getString("HELLO");
         // Ожидаемый результат hello System.out.println(string);
         // Вызовы ваших методов
+        int[] a = new int[4];
+        a[0] = 2;
+        a[1] = 4;
+        a[2] = 5;
+        a[3] = 7;
 
-
-
-        System.out.println();
+        System.out.println(arrayOfNumbers(a));
 
     }
 
@@ -22,18 +25,17 @@ public class Methods {
     }
 
 
-
 // Реализуйте функциональность согласно описанию
 
     // TODO: Создайте метод который вернет сумму двух чисел
-     public static int sum1(int a, int b) {
+    public static int zsum1(int a, int b) {
 
-        return  a  +  b ;
+        return a + b;
     }
 
 
     // TODO: Создайте метод который вернет сумму двух переданных строк
-    public static String sumString(String string, String subString ) {
+    public static String sumString(String string, String subString) {
 
 
         return string.concat(subString);
@@ -45,10 +47,10 @@ public class Methods {
     //  обернуть их в строку и вернуть результат строкой
 
 
-    public static String sumIntegerToStr (String str, int value) {
+    public static String sumIntegerToStr(String str, int value) {
 
 
-        return String.valueOf(Integer.parseInt(str,value));
+        return String.valueOf(Integer.parseInt(str, value));
 
     }
 
@@ -56,20 +58,19 @@ public class Methods {
 // TODO: Создайте метод который принимает строку , число . Внутри строки передается число . Проверить , что если число внутри строки больше чем
 //  число которое передается как int , то пусть метод вернет false , а если меньше , то true
 
-    public static boolean compareStringToInt (String str, int number ) {
+    public static boolean compareStringToInt(String str, int number) {
 
 
-
-        return Integer.parseInt(str)>number ;
+        return Integer.parseInt(str) > number;
     }
 
 
 // TODO: Создайте метод который принимает 2 булевые переменные . Если обе true , верни true , если хоть одна из них true верни true ,
 //  если обе false верни false
 
-    public boolean equals (boolean a, boolean b) {
+    public boolean equals(boolean a, boolean b) {
 
-        return a||b;
+        return a || b;
     }
 
 
@@ -83,20 +84,26 @@ public class Methods {
 // TODO: Создайте метод который проверят числа в массиве , если все они четные верни "Массив четных чисел" , если есть и четные и не
 //  четные верни "Массив четных и не четных чисел" и если в массиве только нечетные числа верни "Массив не четных чисел"
 
-    public static String sum6(int[] array) {
+    public static String arrayOfNumbers(int[] array) {
+        boolean even = false;
+        boolean odd = false;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                even = true;
+            } else {
+                odd = true;
 
-        int max = 6;
-        for (int i : array) {
-            if (i % 2 == 0) {
-                max++;
             }
 
-
         }
-
-        return String.valueOf((max));
+        if (even && odd) {
+            return "Массив четных  и не четных  чиссел ";
+        } else if (even) {
+            return "Массив четных чисел ";
+        } else {
+            return "Массив не четных чиссел ";
+        }
     }
-
 
 // TODO: Создайте метод который принимает массив чисел и возвращает новый массив чисел . Внутри метода пройдитесь по элементам
 //  массива который подается в аргументы  и сложите все элементы в новый массив и верните его
@@ -104,20 +111,21 @@ public class Methods {
 // TODO: Создайте метод который принимает массив чисел и возвращает число . Внутри метода пройдитесь по элементам массива который подается в
 //  аргументы сложите все числа который переданы в массиве и верните сумму всех чисел
 
-    public static String sum7(int[] Array) {
+    public static int sumIntInArray(int[] array) {
+//
+//        int[] a = new int[4];
+//        int sum = 0;
+//        a[0] = 2;
+//        a[1] = 2;
+//        a[2] = 2;
+//        a[3] = 2;
+//        for (int i = 0; i < a.length; i++) {
+//            sum += a[i];
 
-        int[] a = new int[4];
-        int sum = 0;
-        a[0] = 2;
-        a[1] = 2;
-        a[2] = 2;
-        a[3] = 2;
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i];
-        }
 
-        return Arrays.toString(a);
+        return array.length;
     }
+
 
 // TODO: Создайте методы в которые можно будет передавать свой возраст , а возвращаться сколько полных месяцев в этом возрасте ,
 //  тоесть если человек прожил 10 лет , это 120 месяцевтак же сделайте перевод возраста в минуты и метод который расчетает возраст в часах
@@ -125,21 +133,20 @@ public class Methods {
     public static int ageToMonth(int age) {
 
 
-
         return (age * 12);
     }
 
-    public static int  intAgeToMinutes(int age) {
+    public static int intAgeToMinutes(int age) {
 
 
         return (age * 525600);
     }
 
 
-    public static int  intAgeToHours (int age) {
+    public static int intAgeToHours(int age) {
 
 
-        return (age *  8760);
+        return (age * 8760);
     }
 // TODO: Создайте метод которому передаю имена , а он проверяет одинаковые ли они , причем не зависимо от регистра
 
@@ -154,11 +161,7 @@ public class Methods {
     public static int getLastIntInArray(int[] array) {
 
 
-
-
-
-
-        return array[array.length-1] ;
+        return array[array.length - 1];
     }
 
 
@@ -177,7 +180,6 @@ public class Methods {
     }
 
     // TODO: Создайте метод кошелек , у него есть началная сумма и можно вычитать пока деньги не кончатся или сумма не будет слишком велика
-
 
 
     public static String wallet(int summation, int score) {
